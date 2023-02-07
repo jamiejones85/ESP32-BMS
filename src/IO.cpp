e2b7bda80e9f22738a8dff57b5d4c20811070311
Jamie Jones <jamie@mudslidedesign.co.uk>
2023-02-07 10:06:26 +0000

    chaging variables to match style of rest of the codebase

--- src/IO.cpp
+++ src/IO.cpp
@@ -17,22 +17,22 @@ bool IO::getChargeOverride() {
 
 bool IO::isChargeEnabled() {
 
-  int AverageValue = 0;
-  float PP_Voltage =0 ;
-  int MeasurementsToAverage = 16;
-  for(int i = 0; i < MeasurementsToAverage; ++i)
+  int averageValue = 0;
+  float ppVoltage =0 ;
+  int measurementsToAverage = 16;
+  for(int i = 0; i < measurementsToAverage; ++i)
   {
-    AverageValue += analogRead(CHARGE_IN);
+    averageValue += analogRead(CHARGE_IN);
     //delay(1);
   }
-  AverageValue /= MeasurementsToAverage;
-  if(AverageValue < 1 || AverageValue > 4095) return 0;
+  averageValue /= measurementsToAverage;
+  if(averageValue < 1 || averageValue > 4095) return 0;
     //https://github.com/G6EJD/ESP32-ADC-Accuracy-Improvement-function/blob/master/ESP32_ADC_Read_Voltage_Accurate.ino   
     //factor of 2 to corect volatge divider on board  
-    PP_Voltage = int(1000* 2*(-0.000000000000016 * pow(AverageValue,4) + 0.000000000118171 * pow(AverageValue,3)- 0.000000301211691 * pow(AverageValue,2)+ 0.001109019271794 * AverageValue + 0.034143524634089));
+    ppVoltage = int(1000* 2*(-0.000000000000016 * pow(averageValue,4) + 0.000000000118171 * pow(averageValue,3)- 0.000000301211691 * pow(averageValue,2)+ 0.001109019271794 * averageValue + 0.034143524634089));
     // Refer to "J1772_IEC61851.ods" in docs folder for calc For J1772, AC is avaialbe when the voltage on the PP detect pin is between 1.5v and 1.7v to allow for inaccuracies we are going to say between 1.5v and 2.0v
     // above and below this value we will say no ac avaialabe. Note PP_Voltage is mV.
-    return PP_Voltage>1500 && PP_Voltage<2000|| chargeOverride == true;   
+    return ppVoltage>1500 && ppVoltage<2000|| chargeOverride == true;   
 }
 
 bool IO::isDriveEnabled(bool inverterInForwardReverse) {
